7ad2fefbf7c9ce15654dd2c557c80478975dd528
Kumari <akumari53@dxc.com>
2020-07-26 13:17:27 +0530

    my first git

---
+++ localDemoConfig/src/test/java/com/demo/config/localDemoConfig/LocalDemoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.config.localDemoConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalDemoConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
